76643ac9f714339745e6ba16f8f91cb41de09047
Maram Hani Mohammad <maramhanisbaih@gmail.com>
2021-02-28 23:21:02 +0200

    done

--- js/app.js
+++ js/app.js
@@ -1,11 +1,32 @@
-let userName='Cristiano ronaldo';
-let age=30;
-const tax=0.16;
-console.log("before changes: ",userName,age,tax);
-age=50;
-console.log("after changes: ",age,tax);
+//let userName='Maram';
+//let age=25;
 
-alert('Hello everyone');
-confirm('Do you want to continue?');
+//console.log("before changes: ",userName,age,tax);
+//age=50;
+//console.log("after changes: ",age,tax);
 
-let userInput=prompt('enter you fav team: '); 
+//alert('Hey there, press ok');
+//confirm('Do you want to continue?');
+
+
+
+{ 
+    
+    let userName = prompt("Hey there, what's ur name")
+    alert ("Hello " + userName+ " ,we're happy you visited our website");
+    console.log('your name is: ' + userName);
+
+    
+    let using = prompt("Are you using mobile or laptop")
+    alert ("ohh " + using+ " ,sounds great!");
+    console.log('you are using: ' + using);
+
+    
+    let nnet = prompt("now are opining the website using Wifi or you Mobile Data?")
+    alert ("Mee tooo i'm using " + nnet+ " ,yaaaaaaay");
+    console.log('your are opinning from: ' + nnet);
+
+    let media = prompt("What is the most Social Media you Use")
+    alert ("we provide services in " + media + " ,Take a look to our website");
+    console.log('Most SM you use is: ' + media);
+    }
