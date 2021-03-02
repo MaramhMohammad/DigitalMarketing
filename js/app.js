5baf143f637cfc97f08e80e51d54f888b9b1f6d9
Maram Hani Mohammad <maramhanisbaih@gmail.com>
2021-03-02 10:10:51 +0200

    added the js code inside script tag

--- js/app.js
+++ js/app.js
@@ -9,24 +9,3 @@
 //confirm('Do you want to continue?');
 
 
-
-{ 
-    
-    let userName = prompt("Hey there, what's ur name")
-    alert ("Hello " + userName+ " ,we're happy you visited our website");
-    console.log('your name is: ' + userName);
-
-    
-    let using = prompt("Are you using mobile or laptop")
-    alert ("ohh " + using+ " ,sounds great!");
-    console.log('you are using: ' + using);
-
-    
-    let nnet = prompt("now are opining the website using Wifi or you Mobile Data?")
-    alert ("Mee tooo i'm using " + nnet+ " ,yaaaaaaay");
-    console.log('your are opinning from: ' + nnet);
-
-    let media = prompt("What is the most Social Media you Use")
-    alert ("we provide services in " + media + " ,Take a look to our website");
-    console.log('Most SM you use is: ' + media);
-    }
